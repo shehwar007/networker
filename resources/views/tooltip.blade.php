2d83a71f3d01cb2adc173a606e86730900e3fc3d
Shehwar Asif <61156706+shehwar007@users.noreply.github.com>
2023-08-08 13:30:39 +0500

    Done

--- resources/views/tooltip.blade.php
+++ resources/views/tooltip.blade.php
@@ -25,9 +25,11 @@ Definitions:
 ROTI = return on time invested. That is, the short- or long-term value the connection could
 provide verse the time and effort to develop the connection. Value returned could be business
 or introductions to business. Also, consider the likelihood of the connection delivering that return.
+
 ROTI score = Determined by benchmarking the connection against your most valuable client who represents
 the best return on time invested. That client is rated 10/10. Give your connection a ROTI score out of
 10 compared to your benchmark client. A higher ROTI score for a connection is 7-10/10
+
 Relationship score = Determined by benchmarking the connection against your biggest fan.
 That is, the person with whom you have the strongest relationship because you have a lot of chemistry,
 and they find you highly credible. That person is rated 10/10. Give your connection a relationship score 
@@ -39,6 +41,7 @@ Note:
 - If you have just met a connection then clasiify them as a A2 even if the initail contact was very positive.
 You need to meet with them 2 or 3 more times before classifying them as an A1 assuming the chemistry is still
 strong and they still find you credible.
+
 - Your frequency of contact with a strong connection can affect the score. That is, a strong connection may need
 attention if the duration since last contact is over six months.
 
@@ -53,8 +56,11 @@ attention if the duration since last contact is over six months.
     <div style="padding:20px;text-align: left;">
         <pre>
 The indictor will turn red if the duration since you were last in contact with a:
+
 A1 type connection is 6 or more months
+
 A2 type connection is 3 or more months.
+
 In that case there is risk the relationhsip will start to weaken. Therefore make 
 contact by carrying out an appropriate activty. 
 
@@ -68,17 +74,22 @@ contact by carrying out an appropriate activty.
     <div style="padding:20px;text-align: left;">
         <pre>
 Activities on the drop-down list are ordered as follows:
+
 -The activities towards the top half tend to feature direct contact, require more time and effort,
 and have more impact on chemistry and credibility.
+
 -The activities towards the bottom half tend to feature remote contact, require less time and effort,
 and have less impact on chemistry but can build credibility.
 Seminars and webinars tend to be an exception. They take more work and may be stressful, but you have
  more direct contact. This activity also means more opportunity to build chemistry and credibility with
  a higher number of connections.
+
 Also, ensure you choose activity:
+
 - You can realistically complete for all listed A1 and A2 connections over a 1–3-month period. That is,
  avoid trying to do too much given your other commitments.
-- You and the connection enjoy .
+- You and the connection enjoy.
+
 - Represent the best ROTI. Note return could mean immediate business or business over the long-term because 
 it builds a relationship or raises profile. Therefore, be clear on what return is realistic for an activity.
  For instance, the realistic return for presenting a seminar is raising profile rather than immediate business. 
@@ -95,22 +106,30 @@ it builds a relationship or raises profile. Therefore, be clear on what return i
         <pre>
 Effective networkers are always finding ways to help their connections. Therefore ensure your
 activity includes help for the connection. Ways to help include the following:
+
 Connect them to somebody – Can you connect them to a potential client or provider who might resolve a need?
 Maybe you can connect them to others who work in their field or share the same interests?
 Give them insights – Can you give them insights that are relevant to their business, role or
 personal interests? 
+
 Give them a sounding board – Can you spend time with them so they can reflect on their business
 challenges or goals and therefore crystallise their thinking?
+
 Give them something – Is it appropriate to gift them something for Xmas or their birthday etc?
 Personal gifts might relate to their interests e.g., sport, food, or gadgets. Maybe gift something
 relevant to their business role or provide an incentive or reward? 
+
 Give them an opportunity to contribute  – Maybe you are working on a project, and they would appreciate 
 being involved? This might include surveys.
+
 Give them updates - Maybe they would appreciate being kept in the loop on certain developments.
+
 Support them – Can you provide moral or practical support for a project they are working on?
+
 Recognise their contributions or achievements – Acnowledge or reward their contributions where appropriate.
 LinkedIn is a good source for opportunities to recognise achievements such as an award, promotion, or new
 role. Maybe you can provide a testimonial?
+
 
 
 
